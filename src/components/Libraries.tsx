adc8ba45eddbd3a2cbe5000568d80fbf808c801a
Felix <felix.berthier@epita.fr>
2024-06-20 21:02:22 +0200

    Updated all pages sections

--- src/components/Libraries.tsx
+++ src/components/Libraries.tsx
@@ -18,7 +18,7 @@ export function Libraries() {
   return (
     <div className="my-16 xl:max-w-none">
       <Heading level={2} id="official-library">
-        Official library
+        Official libraries
       </Heading>
       <div className="not-prose mt-4 grid grid-cols-1 gap-x-6 gap-y-10 border-t border-zinc-900/5 pt-10 sm:grid-cols-2 xl:max-w-none xl:grid-cols-3 dark:border-white/5">
         {libraries.map((library) => (
