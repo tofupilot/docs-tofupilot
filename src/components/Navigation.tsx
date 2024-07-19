d2263ce1d7eb281ea8172b7dfbae9d07b8040c37
Julien Buteau <julien@strake.one>
2024-07-19 14:40:42 +0200

    Updated public pricing

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -269,10 +269,10 @@ export const navigation: Array<NavGroup> = [
   {
     title: 'Support',
     links: [
-      // { title: 'Pricing', href: '/support/pricing' },
-      // { title: 'Security', href: '/support/security' },
-      { title: 'Privacy Policy', href: '/legal/privacy-policy' },
+      { title: 'Pricing', href: '/support/pricing' },
       { title: 'Terms & Conditions', href: '/legal/terms-conditions' },
+      { title: 'Security', href: '/support/security' },
+      { title: 'Privacy Policy', href: '/legal/privacy-policy' },
     ],
   },
 ]
