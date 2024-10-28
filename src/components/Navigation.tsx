8f6ba5df075114c381b5c06d431dd1e3e0ae4f0e
Charlotte Evéquoz <charlotte.evequoz@tofupilot.com>
2024-10-28 22:22:07 +0100

    WIP: add pictures and rename page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -279,7 +279,7 @@ export const navigation: Array<NavGroup> = [
       { title: 'User Management', href: '/user-management' },
       { title: 'Python Client', href: '/clients/python' },
       { title: 'REST API Reference', href: '/api' },
-      { title: 'Offline data uploads', href: '/asynchronous-uploads' },
+      { title: 'Asynchronous run uploads', href: '/asynchronous-uploads' },
     ],
   },
   {
